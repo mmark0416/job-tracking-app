b16498ad54ab10547487037688ba7b93661055d1
Majoros Jozsef Mark <mmark0416@gmail.com>
2023-11-13 16:18:43 +0100

    complete register page ui

---
+++ client/src/pages/Auth/Register.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import Wrapper from "./Register.wrapper";
+import { FormRow, Logo } from "../../components";
+
+const Register = () => {
+  return (
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type="text" name="name" defaultValue="mark" />
+        <FormRow
+          type="text"
+          name="lastName"
+          labelText="last name"
+          defaultValue="majoros"
+        />
+        <FormRow type="text" name="location" defaultValue="europa" />
+        <FormRow type="text" name="email" defaultValue="mark@gmail.com" />
+        <FormRow type="password" name="password" defaultValue="1234" />
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+        <p>
+          Already a member?
+          <Link to={"/login"} className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  );
+};
+
+export default Register;
