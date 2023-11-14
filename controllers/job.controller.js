96aeb187afc31a8d0247c6f71b5e2afe5e1e6075
Majoros Jozsef Mark <mmark0416@gmail.com>
2023-11-14 18:39:23 +0100

    complete edit job and delete job controllers

--- controllers/job.controller.js
+++ controllers/job.controller.js
@@ -1,16 +1,17 @@
 import Job from '../models/job.model.js';
+import {StatusCodes} from 'http-status-codes';
 
 //Get all jobs
 export const getJobs = async (req, res) => {
   const jobs = await Job.find({});
-  res.status(200).json({jobs});
+  res.status(StatusCodes.OK).json({jobs});
 };
 
 // CREATE JOB
 export const createJob = async (req, res) => {
   const { company, position } = req.body;
   const newJob = await Job.create({ company, position });
-  res.status(201).json({newJob});
+  res.status(StatusCodes.CREATED).json({newJob});
 };
 
 // GET SINGLE JOB
@@ -21,15 +22,30 @@ export const getJob = async (req, res) => {
   if(!job){
     res.status(404).json({ msg: `no job with id ${id}` });
   }
-  res.status(200).json({job});
+  res.status(StatusCodes.OK).json({job});
 };
 
 // EDIT JOB
-export const editJob = (req, res) => {
+export const editJob = async (req, res) => {
+  const { id } = req.params;
 
+  const updatedJob = await Job.findByIdAndUpdate(id, req.body, { new: true });
+
+  if(!updatedJob){
+    res.status(404).json({ msg: `no job with id ${id}` });
+  }
+
+  res.status(StatusCodes.OK).json({msg: "job updated", job: updatedJob});
 };
 
 // DELETE JOB
-export const deleteJob = (req, res) => {
+export const deleteJob = async (req, res) => {
+  const { id } = req.params;
+  const removedJob = await Job.findByIdAndDelete(id);
 
+  if(!removedJob){
+    res.status(404).json({ msg: `no job with id ${id}` });
+  }
+
+  res.status(StatusCodes.OK).json({msg: "job deleted", job: removedJob});
 };
