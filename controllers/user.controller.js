268b2b5b49f718f5b64f31dcb5ccc09e4a081a9b
Majoros Jozsef Mark <mmark0416@gmail.com>
2023-11-20 18:46:57 +0100

    change multer middleware from disk storage to memory storage

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,7 +2,7 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/User.model.js";
 import Job from "../models/Job.model.js";
 import cloudinary from "cloudinary";
-import { promises as fs } from "fs";
+import { formatImage } from "../middleware/multer.middleware.js";
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findById(req.user.userId).select("-password");
@@ -20,8 +20,10 @@ export const updateUser = async (req, res) => {
   delete newUser.password;
 
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path);
+    const file = formatImage(req.file);
+     
+    const response = await cloudinary.v2.uploader.upload(file);
+
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
